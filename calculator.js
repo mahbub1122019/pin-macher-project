1d060135af35e5e5c36bb7284ade941ff17d1bbf
mahbub1122019 <mahbub1122019@gmail.com>
2020-08-04 18:32:13 +0600

    Alhamdulillah

--- calculator.js
+++ calculator.js
@@ -1,13 +1,13 @@
 
-
+//               Bismillahir Rahmanir Rahim
 //Random button Event Handler:::::::::::
 
     function randomButton() {
         document.getElementById("randomButton").addEventListener("click",function(){
-            const random = document.getElementById("randomDisplay").value;
-            const expectedRandom =1000+Math.random(random)*9000;
-            const excellent =Math.round(expectedRandom);
-            document.getElementById("randomDisplay").value=excellent;
+            const randomDisplay = document.getElementById("randomDisplay").value;
+            const random =1000+Math.random(randomDisplay)*9000;
+            const randomNumber =Math.round(random);
+            document.getElementById("randomDisplay").value=randomNumber;
         })
     
     }
@@ -17,13 +17,13 @@ function inputButton(number) {
     var inputNumberAll = number;
     document.getElementById("calculator-input").value+=inputNumberAll;
 }
-// Backbutton event handler::::::::::
+// Backbutton event handler:
 function back() {
     var inputNumber = document.getElementById("calculator-input");
     var inputValue =inputNumber.value;
     document.getElementById("calculator-input").value=inputValue.slice(0,-1);
 }
-// All clear button event handler:::::::::::
+// All clear button event handler:
 
    function clearText()  
 {
@@ -32,19 +32,25 @@ function back() {
 }  
 
 
-//Submit Button event handler"""""""""""""
+//Submit Button event handler:
+
 function notification() {
     document.getElementById("submitButton").addEventListener("click",function(){
-        let randomNumber = document.getElementById("randomDisplay");
-        let calculatorInput= document.getElementById("calculator-input");
-    
-        if (calculator-input=== randomDisplay) {
-            document.getElementById("positive").style.display="block";   
-        } else {document.getElementById("negative").style.display="block"
+        var calculatorInput=document.getElementById("calculator-input");
+        var randomInput=document.getElementById("randomDisplay");
+        if (calculatorInput.value==randomInput.value) {
+          const show =  document.getElementById("positive");
+            show.style.display="block"
+          
+            
+        } else {
+          const alternative =  document.getElementById("negative");
+            alternative.style.display="block";
             
         }
     })
+       
+     
+ }   
+            
     
-    
-}
-
